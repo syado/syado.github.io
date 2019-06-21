f2ca6dc33eec2228a5aea263d940a60cd37a63fa
syado <doi.sinya122@gmail.com>
2019-06-21 10:22:34 +0900

    修正

--- mahjong/service_worker.js
+++ mahjong/service_worker.js
@@ -1,8 +1,8 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'Mahjong_Calculation_App';
 var urlsToCache = [
-    // '/mahjong/hai/',
-    // '/mahjong/image/',
+    '/mahjong/hai/',
+    '/mahjong/image/',
 ];
 
 // インストール処理
